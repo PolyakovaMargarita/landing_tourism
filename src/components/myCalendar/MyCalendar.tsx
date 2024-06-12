82ea53e03b085e719ff5b5d5ec7731deac883d24
PolyakovaMargarita <margaritka251289@gmail.com>
2024-06-12 18:36:12 +0200

    fix navigator

--- src/components/myCalendar/MyCalendar.tsx
+++ src/components/myCalendar/MyCalendar.tsx
@@ -1,21 +1,27 @@
-// @ts-ignore
-import React, { FC, useState } from "react";
+import React, { FC, useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import moment from "moment-timezone";
 import { IntlProvider } from "react-intl";
 import "react-datepicker/dist/react-datepicker.css";
 import s from "./MyCalendar.module.css";
+import {isNotUndefined} from "../../helpers/hoc/isNotUndefined";
 
 interface MyCalendarProps {
-    data: any;
+  data: any;
 }
 
 const MyCalendar: FC<MyCalendarProps> = ({ data }) => {
-  const locale = navigator.language;
-  const timeZone = moment.tz.guess();
-
+  const [locale, setLocale] = useState<string>("en-US"); // default locale
+  const [timeZone, setTimeZone] = useState<string>("UTC"); // default time zone
   const [startDate, setStartDate] = useState<Date | null>(null);
   const [endDate, setEndDate] = useState<Date | null>(null);
+
+  useEffect(() => {
+    if (isNotUndefined) {
+      setLocale(navigator.language);
+      setTimeZone(moment.tz.guess());
+    }
+  }, []);
 
   const handleDateChange = (dates: [Date | null, Date | null]) => {
     const [start, end] = dates;
